3dc2518cb84f30d3e10c14de867f15752d404628
dawidplesniarski <dawidplesniarski@gmail.com>
2019-10-14 18:42:13 +0200

    Basic Charts handling.

--- YourBucks/ViewController.swift
+++ YourBucks/ViewController.swift
@@ -11,9 +11,42 @@ import Charts
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pieChart: PieChartView!
+    
+    var carDataEntry = PieChartDataEntry(value: 0.0)
+    var houseHoldDataEntry = PieChartDataEntry(value: 0.0)
+    
+    var numberOfDownloadsDataEntries = [PieChartDataEntry]()
+    
+    
+    
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        pieChart.chartDescription?.text = ""
+        
+        
+        carDataEntry.value = 50.0
+        houseHoldDataEntry.value = 50.0
+        
+        numberOfDownloadsDataEntries = [carDataEntry,houseHoldDataEntry]
+        updateChartData()
+
+    }
+    
+    func updateChartData() {
+        let chartDataSet = PieChartDataSet(entries: numberOfDownloadsDataEntries, label: nil)
+        let chartData = PieChartData(dataSet: chartDataSet)
+        
+        let colors = [UIColor(named:"pastel_pink"),UIColor(named:"azure")]
+        
+        chartDataSet.colors = colors as! [NSUIColor]
+        
+        pieChart.data = chartData
+        
     }
 
 
