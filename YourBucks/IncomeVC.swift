04af8767e7d1bc61d95958f636ee8cb3f98e2a1f
dawidplesniarski <dawidplesniarski@gmail.com>
2019-10-16 21:26:50 +0200

    New Income View Controller & New Class

---
+++ YourBucks/IncomeVC.swift
@@ -0,0 +1,20 @@
+//
+//  IncomeVC.swift
+//  YourBucks
+//
+//  Created by Dawid on 16/10/2019.
+//  Copyright © 2019 Dawid. All rights reserved.
+//
+
+import UIKit
+
+class IncomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
