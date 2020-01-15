168366100d95ba23ae41a32a8d998c4491e09a8a
dawidplesniarski <dawidplesniarski@gmail.com>
2020-01-15 11:39:33 +0100

    pickerView filled with stocks symbols.

--- YourBucks/SelectStockViewController.swift
+++ YourBucks/SelectStockViewController.swift
@@ -8,15 +8,42 @@
 
 import UIKit
 
-class SelectStockViewController: UIViewController {
-
+class SelectStockViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var pickerBlurEffect: UIVisualEffectView!
+    @IBOutlet weak var buttonBlurEffect: UIVisualEffectView!
+    
+    let stocksSymbols:[String] = ["AAPL","MSFT","SBUX","AMZN","NKE","EBAY","JAGX"]
+    var stockSymbol:String = "AAPL"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func buttonTapped(_ sender: Any) {
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return stocksSymbols.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return stocksSymbols[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        stockSymbol = stocksSymbols[row]
+        print(stockSymbol)
+    }
+    
     /*
     // MARK: - Navigation
 
